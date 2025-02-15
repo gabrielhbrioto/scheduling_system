0402d2a49b03604b36079f435833ae0401b2237b
gabrielhbrioto <gabriel.h.brioto@gmail.com>
2025-02-14 22:48:09 -0300

    Adicionando o código da aplicação

---
+++ scheduling_system/scheduling_system/doctype/appointment/appointment.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Gabriel Henrique Brioto and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Appointment", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ scheduling_system/scheduling_system/doctype/appointment/appointment.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Gabriel Henrique Brioto and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Appointment(Document):
+	pass
